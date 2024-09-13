38190d7cafbabca97524cb5d36b32cdf0484aef7
Dinesh <naik96657@gmail.com>
2024-09-13 19:49:44 +0530

    god git repo imported called func

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dknaik/puppy
 
 go 1.22.5
+
+require github.com/dknaik/dogs v0.0.0-20240913140730-218de60a20ea // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/dknaik/dogs"
+)
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof! woof! woof!"
+}
+
+func BigBark() string {
+	return dogs.WhenGrowsUp(Bark())
+}
+
+func BigBarks() string {
+	return dogs.WhenGrowsUp(Barks())
 }
